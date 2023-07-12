fa4ca5ce0ac4e90d8e259e05a39bfdd39ca583a1
Sergio7523 <orlovsergey263759223@gmail.com>
2023-07-12 17:43:51 +0300

    измененена фкнкция гороскопа, добавлены параметры запроса и изменен словарь ссылок

--- constants.py
+++ constants.py
@@ -8,22 +8,22 @@ load_dotenv()
 WEATHER_URL = 'http://wttr.in/'
 CAT_URL = 'https://api.thecatapi.com/v1/images/search'
 DOG_URL = 'https://api.thedogapi.com/v1/images/search'
-HOROSCOPE_URL = 'https://1001goroskop.ru/?znak='
+HOROSCOPE_URL = 'https://1001goroskop.ru/'
 CURRENCIES_URL = 'https://api.coingate.com/v2/rates/merchant/'
 
-HOROSCOPE_SIGNS_URLS = {
-    'овен': f'{HOROSCOPE_URL}aries',
-    'телец': f'{HOROSCOPE_URL}taurus',
-    'близнецы': f'{HOROSCOPE_URL}gemini',
-    'рак': f'{HOROSCOPE_URL}cancer',
-    'лев': f'{HOROSCOPE_URL}leo',
-    'дева': f'{HOROSCOPE_URL}virgo',
-    'весы': f'{HOROSCOPE_URL}libra',
-    'скорпион': f'{HOROSCOPE_URL}scorpio',
-    'стрелец': f'{HOROSCOPE_URL}sagittarius',
-    'козерог': f'{HOROSCOPE_URL}capricorn',
-    'водолей': f'{HOROSCOPE_URL}aquarius',
-    'рыбы': f'{HOROSCOPE_URL}pisces',
+HOROSCOPE_SIGNS = {
+    'овен': 'aries',
+    'телец': 'taurus',
+    'близнецы': 'gemini',
+    'рак': 'cancer',
+    'лев': 'leo',
+    'дева': 'virgo',
+    'весы': 'libra',
+    'скорпион': 'scorpio',
+    'стрелец': 'sagittarius',
+    'козерог': 'capricorn',
+    'водолей': 'aquarius',
+    'рыбы': 'pisces',
 }
 
 USERS = {}
