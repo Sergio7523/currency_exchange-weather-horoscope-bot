dc36738557a3d0a50f94cd5d7033f770b6d23653
Sergio7523 <orlovsergey263759223@gmail.com>
2023-07-26 11:41:02 +0300

    измененена константа ALLOWED_USERS

--- constants.py
+++ constants.py
@@ -26,7 +26,7 @@ HOROSCOPE_SIGNS = {
     'рыбы': 'pisces',
 }
 
-ALLOWED_USERS = os.getenv('ALLOWED_USERS').split(', ')
+ALLOWED_USERS = tuple(map(str.strip, os.getenv('ALLOWED_USERS').split(',')))
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('POSTGRES_PASSWORD')
